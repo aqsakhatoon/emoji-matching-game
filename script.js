8732a6b8f1a2f1470c44f796a701e501c5aa2a04
aqsakhatoon <khanabrar_02@yahoo.com>
2022-09-04 22:05:59 -0500

    base html

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // I decided to go with a DOM game because even though it is  a bit difficult for me to understand, but canvas was even more difficult.
+
+})
