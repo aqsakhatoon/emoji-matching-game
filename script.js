dfe7d917dd17b7b8dd9f193477cbc015168fd26e
aqsakhatoon <khanabrar_02@yahoo.com>
2022-09-06 06:36:43 -0500

    let score

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let compare = [];
     //I am using indexes so when a pair of emojis does not match, they can hide again.
     let indexes = [];
-    
+    //The 'score' and 'count' are being used to keep the count of how many emojis you have matched, and as soon as you have got all 12, the timer will stop.
+    let score = 0;
+    let count = 0;
 })
