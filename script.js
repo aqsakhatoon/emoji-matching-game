6008afde66aab91d4cc90796989df7fcf91fd001
aqsakhatoon <khanabrar_02@yahoo.com>
2022-09-06 09:10:59 -0500

    arr i

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let i = arr.length - 1; i > 0; i -- ) {
             const x = Math.floor(Math.random() * (i + 1));
             const temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
 })
