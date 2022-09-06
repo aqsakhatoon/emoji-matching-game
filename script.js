e9691b25c9f57ff2d548050e31899e3bea696dbd
aqsakhatoon <khanabrar_02@yahoo.com>
2022-09-06 10:33:57 -0500

    hashtag for div id

--- script.js
+++ script.js
@@ -38,4 +38,13 @@ function createBoard() {
     }
 }
 
+function myFunction() {
+    var x = document.getElementById(box1);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
+
 
