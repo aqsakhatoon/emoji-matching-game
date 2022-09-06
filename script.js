4dcaba5203768d14ef64e90d8cdea640db149233
aqsakhatoon <khanabrar_02@yahoo.com>
2022-09-05 20:57:28 -0500

    box js

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ document.addEventListener('DOMContentLoaded', () => {
     // I decided to go with a DOM game because even though it is  a bit difficult for me to understand, but canvas was even more difficult.
 
     const emojis = "`😀`, `😀`, `🧐`, `🧐`, `🥳`, `🥳`, `🥹`, `🥹`, `😁`, `😁`, `😂`, `😂`"
+    const boxes = document.querySelectorAll('.box');
 })
